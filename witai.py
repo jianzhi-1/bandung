f809f2fa8f20d117bf1bc9953d44a7654056d908
jdings <88555216+jdings@users.noreply.github.com>
2021-08-07 01:44:41 +0800

    implemented wit bot class

--- witai.py
+++ witai.py
@@ -1,13 +1,94 @@
 from wit import Wit
+import datetime
 import logging
+import requests
 import os
+from functools import cmp_to_key
 
-access_token = os.environ['WIT_AI_ACCESS_TOKEN']
+#access_token = os.environ['WIT_AI_ACCESS_TOKEN']
 
-client = Wit(access_token)
-resp = client.message('Hi Boss')
-print(type(resp), flush=True)
-print('Yay, got Wit.ai response: ' + str(resp), flush=True)
+def cmp(a, b):
+    if (a['confidence'] == b['confidence']): return 0
+    elif (a['confidence'] < b['confidence']): return 1
+    else: return -1
+
+class WitBot:
+    def __init__(self):
+        self.access_token = os.environ['WIT_AI_ACCESS_TOKEN']
+        self.client = Wit(self.access_token)
+    
+    def query(self, msg):
+        resp = self.client.message(msg)
+        cmp_key = cmp_to_key(cmp)
+        resp['intents'].sort(key=cmp_key)
+
+        if (len(resp['intents']) == 0):
+            return (False, "Sorry. No good suggestions")
+        else:
+            if (resp['intents'][0]['confidence'] > 0.7):
+                return (True, (resp['intents'][0]['name'], resp['intents'][0]['confidence']))
+            else:
+                if (len(resp['intents']) > 1):
+                    return (False, "No recipient indicated. Do you mean to send to {} ({} sure) or {} ({} sure)?".format(resp['intents'][0]['name'], str(int(100.0*resp['intents'][0]['confidence'])) + "%", resp['intents'][1]['name'], str(int(100.0*resp['intents'][1]['confidence'])) + "%"))
+                else:
+                    return (False, "No recipient indicated. Do you mean to send to {} ({} sure)?".format(resp['intents'][0]['name'], str(int(100.0*resp['intents'][0]['confidence'])) + "%"))
+        return resp['intents']
+
+    def _query(self, msg):
+        resp = self.client.message(msg)
+        cmp_key = cmp_to_key(cmp)
+        resp['intents'].sort(key=cmp_key)
+        return resp['intents']
+
+    def create_new_intent(self, intent_name):
+
+        #Definition
+        #POST https://api.wit.ai/intents
+        #Example request
+        #$ curl -XPOST 'https://api.wit.ai/intents?v=20200513' \
+        #-H "Authorization: Bearer $TOKEN" \
+        #-H "Content-Type: application/json" \
+        #-d '{"name": "buy_flowers"}'
+        #Example response
+        #{
+        #"id": "13989798788",
+        #"name": "buy_flowers"
+        #}
+
+    
+    def train_intent(self, intent_name, text):
+
+        #Definition
+        #POST https://api.wit.ai/utterances
+        #Example request
+        #$ curl -XPOST 'https://api.wit.ai/utterances?v=20200513' \
+        #-d '[{
+        #        "text": "I want to fly to sfo",
+        #        "intent": "flight_request",
+        #        "entities": [
+        #        {
+        #            "entity": "wit$location:to",
+        #            "start": 17,
+        #            "end": 20,
+        #            "body": "sfo",
+        #            "entities": []
+        #        }
+        #        ],
+        #        "traits": []
+        #    }]'
+        #Example response
+        #{
+        #"sent": true,
+        #"n": 1
+        #}
+
+
+        return
+
+#client = Wit(access_token)
+#resp = client.message('Hi Boss')
+#print(type(resp), flush=True)
+#print('Yay, got Wit.ai response: ' + str(resp), flush=True)
 
 # client.create_intent(intent_name)
 
